e4580ba0dfd6ab05b89086f09bfe1b023fd0a6b2
jahanzebNawaz <jahanzaib.ch9996@gmail.com>
2021-12-10 12:19:57 -0500

    setting updated

--- TODO/TODO/settings.py
+++ TODO/TODO/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9%+triaw$eb4%up&i83^m)wv3arl^(i79ia5pzr9$k8(cm5hs4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,9 +130,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR /"static"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR /"media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
